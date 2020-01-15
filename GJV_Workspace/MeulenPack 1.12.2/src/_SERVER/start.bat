3779795c82357dad17ba6b95662834f29fbca359
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2020-01-15 12:50:47 +0100

    Add windows startup script

---
+++ GJV_Workspace/MeulenPack 1.12.2/src/_SERVER/start.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting MeulenServer 1.12.2!
+echo.
+:loop
+java -server -Xms1g -Xmx10g -jar forge-1.12.2-14.23.4.2757-universal.jar nogui
+echo.
+echo Server stopped or crashed, restarting in 10 seconds
+TIMEOUT /T 10 /NOBREAK
+goto loop
