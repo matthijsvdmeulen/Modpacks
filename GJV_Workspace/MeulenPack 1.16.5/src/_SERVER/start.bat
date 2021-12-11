156efc4eaf68b26516b1608824d5a407d49200f0
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2021-12-11 16:56:56 +0100

    Update forge (log4j RCW)

--- GJV_Workspace/MeulenPack 1.16.5/src/_SERVER/start.bat
+++ GJV_Workspace/MeulenPack 1.16.5/src/_SERVER/start.bat
@@ -2,7 +2,7 @@
 echo Starting MeulenServer 1.16.5!
 echo.
 :loop
-java -server -Xms1g -Xmx10g -jar -Dfml.readTimeout=60 forge-1.16.5-36.2.0.jar nogui
+java -server -Xms1g -Xmx10g -jar -Dfml.readTimeout=60 forge-1.16.5-36.2.20.jar nogui
 echo.
 echo Server stopped or crashed, restarting in 10 seconds
 TIMEOUT /T 10 /NOBREAK
