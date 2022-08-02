308ed25bbc7efea2ceb6d0b07a3757f5a2d4ca99
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2022-08-02 23:21:16 +0200

    Update forge on server

--- GJV_Workspace/MeulenPack 1.12.2/src/_SERVER/start.bat
+++ GJV_Workspace/MeulenPack 1.12.2/src/_SERVER/start.bat
@@ -2,7 +2,7 @@
 echo Starting MeulenServer 1.12.2!
 echo.
 :loop
-java -server -Xms1g -Xmx10g -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
+java -server -Xms1g -Xmx10g -jar forge-1.12.2-14.23.5.2860.jar nogui
 echo.
 echo Server stopped or crashed, restarting in 10 seconds
 TIMEOUT /T 10 /NOBREAK
--- GJV_Workspace/MeulenPack 1.12.2/src/_SERVER/start.sh
+++ GJV_Workspace/MeulenPack 1.12.2/src/_SERVER/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-	java -server -Xms1g -Xmx6g -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
+	java -server -Xms1g -Xmx10g -jar forge-1.12.2-14.23.5.2860.jar nogui
 	sleep 5
 done
